3c31fe5de81de96b497ae27b20d8c9624cff390d
sugat <sugatmoon17@gmail.com>
2023-07-03 22:15:07 +0530

    Final varible:variable can by initilized at the time of decleration, by usign constutctor, a instance block Final Method: Methode can not be further change, they only can use. Final Class : Class can not be extends further.

---
+++ finalKeyword.java
@@ -0,0 +1,21 @@
+
+/*final*/ class parent {
+    /*final*/  void methode () {
+        System.out.println("Parent");
+    }
+}
+class child1 extends parent {
+    public void methode() {
+        System.out.println("Child");
+    }
+}
+public class finalKeyword {
+        final double PI ;
+         {
+            PI = 54.21;
+        }
+    public static void main(String[] args) {
+
+
+    }
+}
